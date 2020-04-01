52a95e22930551a03624a7ca7a14472f4d83b9d9
BennaceurHichem <gh_bennaceur@esi.dz>
2020-04-01 07:45:27 +0100

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+
+
+//home is a container of all the Poll list(answred and unanswered)
+export default class Home extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
---
+++ src/components/Poll.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+//an elemnt of the list of polls shown in the Home page 
+export default class Poll extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
